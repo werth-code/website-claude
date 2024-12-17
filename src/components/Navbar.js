1c6fd6d19790bb8c1efba412f45b2c58455a557f
Matthew Werth <55985843+werth-code@users.noreply.github.com>
2024-12-17 08:53:52 -0500

    Implement Klipsan-inspired fitness website design with dark theme and modern layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
+      <div className="logo">KLIPSAN</div>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/pricing">Pricing</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
